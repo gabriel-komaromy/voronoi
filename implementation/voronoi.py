1ac6d352eebacb4628e478fb1329391586da9c49
g-ewing <gre5@case.edu>
2016-04-27 14:32:21 -0400

    implemented queue as deletion set
    Signed-off-by: g-ewing <gre5@case.edu>

--- implementation/voronoi.py
+++ implementation/voronoi.py
@@ -1,4 +1,4 @@
-from Queue import PriorityQueue
+import heapq
 
 
 class Point(object):
@@ -13,6 +13,9 @@ class Point(object):
             return self.y - other.y
         else:
             return self.x - other.x
+
+    def __eq__(self, other):
+        return self.y == other.y and self.x == other.x
 
     def __str__(self):
         return "(" + str(self.x) + ", " + str(self.y) + ")"
@@ -29,7 +32,32 @@ class LineSegment(object):
 
 def create_diagram(points):
     # Lexicographical ordering of the data points
-    ordered_points = PriorityQueue()
-    for point in points:
-        ordered_points.put(point)
-    return ordered_points
+    ordered_points = list(points)
+    heapq.heapify(ordered_points)
+    deleted_points = set()
+    deleted_points = delete_point(Point(1, 0), deleted_points)
+    print_undeleted_points(ordered_points, deleted_points)
+
+
+def next_point(ordered_points, deleted_points):
+    while True:
+        popped_point = heapq.heappop(ordered_points)
+        if not is_deleted(popped_point, deleted_points):
+            break
+    return popped_point
+
+
+def is_deleted(point, deleted_points):
+    return str(point) in deleted_points
+
+
+def delete_point(point, deleted_points):
+    deleted_points.add(str(point))
+    return deleted_points
+
+
+def print_undeleted_points(point_list, deleted_points):
+    while len(point_list) > 0:
+        next_point = heapq.heappop(point_list)
+        if not is_deleted(next_point, deleted_points):
+            print next_point
