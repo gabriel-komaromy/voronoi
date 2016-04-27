fd0caa89ccce3186df245d8df3748c76333e30a8
g-ewing <gre5@case.edu>
2016-04-26 22:29:39 -0400

    created priority queue for input points
    Signed-off-by: g-ewing <gre5@case.edu>

--- implementation/voronoi.py
+++ implementation/voronoi.py
@@ -1,7 +1,18 @@
+from Queue import PriorityQueue
+
+
 class Point(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def __cmp__(self, other):
+        """The sweepline moves up the plane, so we consider points
+        with lower y first, and break ties in y by lower x first"""
+        if self.y != other.y:
+            return self.y - other.y
+        else:
+            return self.x - other.x
 
     def __str__(self):
         return "(" + str(self.x) + ", " + str(self.y) + ")"
@@ -9,8 +20,16 @@ class Point(object):
 
 class LineSegment(object):
     def __init__(self, endpoints):
+        assert len(endpoints) == 2
         self.endpoints = endpoints
+
+    def __str__(self):
+        return str(self.endpoints[0]) + " to " + str(self.endpoints[1])
 
 
 def create_diagram(points):
-    pass
+    # Lexicographical ordering of the data points
+    ordered_points = PriorityQueue()
+    for point in points:
+        ordered_points.put(point)
+    return ordered_points
